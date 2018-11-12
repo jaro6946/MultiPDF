e842ebda253a6055bf1ed12442f927796ff158c1
Jacob Roberson <35666891+jaro6946@users.noreply.github.com>
2018-11-12 14:56:50 -0500

    Update MultiPDF.py

--- MultiPDF.py
+++ MultiPDF.py
@@ -1,28 +1,6 @@
 #MultiPDF
 #Version 1.0
 
-
-#These functions allows you to fill PDFs that have fillable fields using an excel spreadsheet.  
-#
-# If you need to fill 20 PDFs with different information, this module should work 
-#   with some stipulations
-
-#Limitations: You must open your completed PDFs with Mac Preview.  Adobe products will
-#             not work.  
-#             If you are getting errors regarding encryption, you may have to use
-#             https://smallpdf.com/unlock-pdf to unlock the PDF
-
-
-#Methods:
-#get_field_keys(input_pdf_path) 
-#   This makes a CSV template that can be imported into
-#   excel that you can fill with your information.  This spread sheet will then be exported
-#   as a CSV and then used as an argument in the write_fillable_pdf method.    
-
-#write_fillable_pdfs(input_CSV, input_pdf_template)
-#   This method will write your PDF files as specifde in the input_CSV
-
-
 #! /usr/bin/python
 
 import os
@@ -197,8 +175,3 @@ def write_fillable_pdfs(input_CSV, input_pdf_template):
 
 
 
-if __name__ == '__main__':
-    #get_field_keys(INVOICE_TEMPLATE_PATH)
-    #write_fillable_pdf(INVOICE_TEMPLATE_PATH, INVOICE_OUTPUT_PATH, data_dict)
-    #write_fillable_pdf('PDF_Fields.csv','DA-31-unlocked.pdf')
-    print('lol')
